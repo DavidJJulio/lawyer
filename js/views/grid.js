c5af08389995fe2b2cd2e2b96fbf832e18189953
DavidJJulio <158339997+DavidJJulio@users.noreply.github.com>
2024-07-10 11:04:46 -0500

    feat: :construction: Structuring with grid

--- js/views/grid.js
+++ js/views/grid.js
@@ -9,37 +9,86 @@ export class grid extends HTMLElement {
                 padding: 0;
                 box-sizing: border-box;
             }
-            main {
-                display: block;
+            main{
+                display: flex;
+                background-color: gainsboro;
+                flex-direction: column;
+                height: 500vh;
                 width: 100%;
-                height: 100vh;
-                background-color: black;
             }
-            .nav_header{
+
+            .hitch {
+                display: grid;
                 width: 100%;
-                height: 20vh;
+                height: 85vh;
                 background-color: blue;
+                grid-template-columns: 1fr;
+                grid-template-rows: 1fr 2fr 12fr;
+                grid-template-areas: 
+                "links"
+                "header"
+                "container"
+                ;   
             }
-            .nav_links{
-                width: 100%;
-                height: 10vh;
+
+            .nav_links {
+                grid-area: links;
                 background-color: antiquewhite;
+            }
+
+            .nav_header {
+                grid-area: header;
+                background-color: aqua;
+            }
+
+            .img_Container {
+                grid-area: container;
+                background-color: rebeccapurple;
+            }
+            .about {
+                width: 100%;
+                height: 40vh;
+                display: flex;
+            }
+
+            .about_me {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                flex-direction: column;
+                width: 50%;
+                height: 100%;
+                background-color: brown;
+            }
+            .my_commitment {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                flex-direction: column;
+                width: 50%;
+                height: 100%;
+                background-color: bisque;
             }
 
         </style>
         <main>
-            <section class ="Hitch">
-                <header>
-                    <nav class ="nav_links"></nav>
-                    <nav class ="nav_header"></nav>
-                </header>
-                <div class ="img_Container">
+            <section class ="hitch">
+                <nav class ="nav_links"></nav>
+                <nav class ="nav_header"></nav>
+                <div class ="img_container">
                 
                 </div>
 
             </section>
 
-            <section></section>
+            <section class ="about">
+            <div class ="about_me">
+                
+            </div>
+            <div class ="my_commitment">
+            
+            </div>
+            </section>
             <section></section>
             <section></section>
             <section></section>
