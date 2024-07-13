cac2a2da0f5aaccd2ffcb88a4366496859c7a317
DavidJJulio <158339997+DavidJJulio@users.noreply.github.com>
2024-07-13 18:32:17 -0500

    feat: :sparkles: nav_links done

--- js/views/grid.js
+++ js/views/grid.js
@@ -3,11 +3,13 @@ export class grid extends HTMLElement {
         super();
         this.attachShadow({mode: "open"})
         this.shadowRoot.innerHTML += /*html*/`
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
         <style>
             * {
                 margin: 0;
                 padding: 0;
                 box-sizing: border-box;
+                font-family: Arial, Helvetica, sans-serif;
             }
             main{
                 display: flex;
@@ -21,7 +23,6 @@ export class grid extends HTMLElement {
                 display: grid;
                 width: 100%;
                 height: 85vh;
-                background-color: blue;
                 grid-template-columns: 1fr;
                 grid-template-rows: 1fr 2fr 12fr;
                 grid-template-areas: 
@@ -33,18 +34,37 @@ export class grid extends HTMLElement {
 
             .nav_links {
                 grid-area: links;
-                background-color: antiquewhite;
+                display: flex;
+                gap: 20px;
+                justify-content: flex-end;
+                padding-right: 20%;
+                padding-left: 20%;
+                background-color: #ebebeb;
             }
 
             .nav_header {
                 grid-area: header;
-                background-color: aqua;
+                background-color: azure;
             }
 
-            .img_Container {
+            .img_container {
                 grid-area: container;
-                background-color: rebeccapurple;
+                display: flex;
+                background-size: cover;
+                background-position: center;
+                background-repeat: no-repeat;
+                visibility: inherit;
+                opacity: 0.8;
+                background-color: #792b4e; 
+                background-image: url(../../storage/img/7.jpg);
             }
+            .img_container::after {
+                background-color: #792b4e41;
+                content: '';
+                width: 100%;
+                height: 100%;
+            }
+            
             .about {
                 width: 100%;
                 height: 45vh;
@@ -140,13 +160,97 @@ export class grid extends HTMLElement {
                 height: 100%;
                 background-color: black;
             }
+
+            li {
+                list-style: none;
+                display: flex;
+                flex-direction: row;
+                gap: 3px;
+                justify-content: center;
+                align-items: center;
+            }
+
+            a {
+                text-decoration: none;
+            }
+
+            .nav_links i {
+                color: #949494;
+            }
+
+            .nav_links a {
+                font-size: 85%;
+                color: #792b4e;
+            }
+
+            .nav_links p {
+                font-size: 85%;
+                color: #949494;
+            }
+
+            .nav_links a:hover {
+                color: #c7608e;
+            }
+
+            .nav_links i:hover {
+                color: #792b4e;
+            }
+
+            .li_socia_media {
+                display: flex;
+                gap: 7px;
+            }
+
+            .a_social_media {
+                border-radius: 100%;
+                width: 30px;
+                height: 30px;
+                background-color: white;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }
+
+            .a_social_media i {
+                font-size: 1.4em;
+                width: 100%;
+                height: 100%;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }
+
+            .a_social_media:hover {
+                background-color: #792b4e;
+            }
+            .a_social_media i:hover {
+                color: white;
+            }
+
         </style>
         <main>
             <section class ="hitch">
-                <nav class ="nav_links"></nav>
+                <nav class ="nav_links">
+                <li>
+                    <i class='bx bx-current-location'></i>
+                    <p>Bucaramanga</p>
+                </li>
+                <li>
+                    <i class='bx bxs-envelope'></i>
+                    <a>pepealcachofa.example@gmail.com</a>
+                </li>
+                <li>
+                    <i class='bx bxs-phone' ></i>
+                    <a>+57 310 3103 310</a>
+                </li>
+                <li class ="li_socia_media">
+                <a class ="a_social_media"><i class='bx bxl-facebook' ></i></a>
+                <a class ="a_social_media"><i class='bx bxl-instagram-alt'></i></a>
+                </li>
+                </nav>
                 <nav class ="nav_header"></nav>
                 <div class ="img_container">
-                
+                   
                 </div>
             </section>
 
@@ -166,7 +270,6 @@ export class grid extends HTMLElement {
                     <p></p>
                     </div>
                     <div class ="practice_cards">
-                    
                     </div>
                 </div>
             </section>
